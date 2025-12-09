471aa25fb4639b6b4ff30d2430cf319053b598e3
Txinto Vaz <txinto@gmail.com>
2025-12-09 21:07:40 +0000

    Measurement relays on new NetVars nvs managers

--- components/Measurement/Measurement_netvars.c
+++ components/Measurement/Measurement_netvars.c
@@ -7,25 +7,11 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 
-const static char TAG[] = "Measurement_netvars";
-
 // La instancia de datos real debe estar definida en otro lugar, por ejemplo:
 // MEASUREMENT_t MEASUREMENT;
 extern Measurement_dre_t Measurement_dre;
 
-static SemaphoreHandle_t s_nvs_mutex = NULL;
-static bool s_nvs_dirty = false;
-static TickType_t s_nvs_dirty_since = 0;
-
-static inline BaseType_t _create_nvs_mutex_once(void)
-{
-    if (!s_nvs_mutex)
-    {
-        s_nvs_mutex = xSemaphoreCreateMutex();
-        if (!s_nvs_mutex) return pdFAIL;
-    }
-    return pdPASS;
-}
+static netvars_nvs_mgr_t Measurement_nvs_mgr = {0};
 
 const NetVars_desc_t Measurement_netvars_desc[] = {
 #include "Measurement_netvars_fragment.c_"
@@ -54,35 +40,21 @@ void Measurement_netvars_nvs_save(void)
 void Measurement_config_parse_json(const char *data)
 {
     bool nvs_cfg_changed = NetVars_parse_json_component_data("Measurement", Measurement_netvars_desc, Measurement_netvars_count, data);
-        if (nvs_cfg_changed)
-        {
-            Measurement_nvs_set_dirty();
+    if (nvs_cfg_changed)
+    {
+        Measurement_nvs_set_dirty();
     }
 }
 
 
 void Measurement_nvs_set_dirty(void)
 {
-    if (_create_nvs_mutex_once() != pdPASS) return;
-    xSemaphoreTake(s_nvs_mutex, portMAX_DELAY);
-    s_nvs_dirty = true;
-    s_nvs_dirty_since = xTaskGetTickCount();
-    xSemaphoreGive(s_nvs_mutex);
+    NetVars_nvs_set_dirty(&Measurement_nvs_mgr);
 }
 
 void Measurement_nvs_spin(void)
 {
-    if (_create_nvs_mutex_once() != pdPASS) return;
-    TickType_t now_ticks = xTaskGetTickCount();
-    bool should_save = false;
-    xSemaphoreTake(s_nvs_mutex, portMAX_DELAY);
-    if (s_nvs_dirty && (TickType_t)(now_ticks - s_nvs_dirty_since) >= pdMS_TO_TICKS(5000))
-    {
-        s_nvs_dirty = false;
-        should_save = true;
-    }
-    xSemaphoreGive(s_nvs_mutex);
-    if (should_save)
+    if (NetVars_nvs_spin(&Measurement_nvs_mgr))
     {
         Measurement_netvars_nvs_save();
     }
